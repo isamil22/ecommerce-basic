e92e088e983631ee26172b10a769ae7a619c3150
isamil22 <yesofcourse2024@gmail.com>
2025-07-25 19:10:08 +0100

    seo react-snap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
 // frontend/src/main.jsx
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client'; // Import hydrateRoot
 import './index.css';
 import App from './App.jsx';
-import { HelmetProvider } from 'react-helmet-async'; // <-- ADD THIS LINE
+import { HelmetProvider } from 'react-helmet-async';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
 
-// Render the App component without StrictMode to resolve the warning
-root.render(
+// Use hydrateRoot to attach React to the pre-rendered HTML
+hydrateRoot(
+    container,
     <React.Fragment>
-        <HelmetProvider> {/* <-- WRAP YOUR APP */}
+        <HelmetProvider>
             <App />
         </HelmetProvider>
     </React.Fragment>
